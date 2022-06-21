569d42ea9e2b9d25dc40a755d59c713de7d93b6a
Devdutt Rai <devduttrai03@gmail.com>
2022-06-21 11:24:11 +0530

    first commit

--- DataTypes.dart
+++ DataTypes.dart
@@ -2,5 +2,9 @@ void main() {
   String courseName = "Flutter Course";
   String trainingName = "Hybrid Mobile App Development";
   print("$trainingName : $courseName"); //concatination
-  print(trainingName + " : " + courseName); 
+  print(trainingName + " : " + courseName);
+
+  int distance = 200;
+  print(distance);
+  print("Distance form Delhi to GLA is: $distance kms");
 }
