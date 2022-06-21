b6bd2f24447a7205deae9fedf929febe6ec30700
Devdutt Rai <devduttrai03@gmail.com>
2022-06-21 11:17:14 +0530

    first commit

---
+++ DataTypes.dart
@@ -0,0 +1,6 @@
+void main() {
+  String courseName = "Flutter Course";
+  String trainingName = "Hybrid Mobile App Development";
+  print("$trainingName : $courseName"); //concatination
+  print(trainingName + " : " + courseName); 
+}
