f7fa056d7d0c3ad40e4f72ab9b5a7f0e9c4bd467
Devdutt Rai <devduttrai03@gmail.com>
2022-06-28 12:25:30 +0530

    first commit

---
+++ Operators.dart
@@ -0,0 +1,9 @@
+void main() {
+/* Dart supprts the following Operators:
+  1)Arithmetic Operators
+  2)Assignment Operators
+  3)Relational Operators
+  4)Logical Operators
+  5)Conditional Operators
+ */
+}
