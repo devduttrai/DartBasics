f49def4fa2f7bad5f696ac3011e47e338ee78d33
Devdutt Rai <devduttrai03@gmail.com>
2022-06-28 22:38:53 +0530

    first commit

--- If-ElseStatement.dart
+++ If-ElseStatement.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("20 + 15: ");
-  int num = int.parse(stdin.readLineSync()!);
+  //stdout.write("20 + 15: ");
+  //int num = int.parse(stdin.readLineSync()!);
 
-  if (num == 35) {
-    print("Result correct");
-  } else {
-    print("Result incorrect");
+  //if (num == 35) {
+  //print("Result correct");
+  //} else {
+  //print("Result incorrect");
+  //}
+
+  stdout.write("Hour: ");
+  int hour = int.parse(stdin.readLineSync()!);
+
+  if (hour < 10) {
+    print("Good Morning");
+  } else if (hour < 20) {
+    print("Good Day");
+  }else{
+    print("Good Night");
   }
 }
