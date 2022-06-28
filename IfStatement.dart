02028b8386f5385b8e3f4157208a525e816838e7
Devdutt Rai <devduttrai03@gmail.com>
2022-06-28 13:15:37 +0530

    first commit

---
+++ IfStatement.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("20 + 15: ");
+  int num = int.parse(stdin.readLineSync()!);
+
+  if (num == 35) {
+    print("Result correct");
+  }
+}
--- InputData.dart
+++ InputData.dart
@@ -1,7 +1,12 @@
 import 'dart:io';
 
 void main() {
-  print("Enter your name: ");
-  String? name = stdin.readLineSync();
-  print(name);
+  //print("Enter your name: ");
+  //String? name = stdin.readLineSync();
+  //print(name);
+  print("Enter first number");
+  var num1 = stdin.readLineSync();
+  print("Enter second number");
+  var num2 = stdin.readLineSync();
+  print(num1! + num2!);
 }
